64683762dcfa3d6ae8b512e1997c490c500de596
Charan-bavaji <charan.r7760@gmail.com>
2023-11-06 19:28:07 +0530

    social media flowting feature

---
+++ src/Components/Certificates.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Certificates = () => {
+
+  return (
+    <div>
+      <a href="../assets/CharanRES.pdf" download>CV</a>
+    </div>
+  );
+}
+
+export default Certificates
