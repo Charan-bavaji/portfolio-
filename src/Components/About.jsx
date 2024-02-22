ef1200dd90b3d9e8c0554c1609948270b2f8bbcb
Charan-bavaji <charan.r7760@gmail.com>
2024-02-22 13:46:02 +0530

    Designed Footer

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -51,10 +51,19 @@ const About = () => {
                         <h1 className='text-3xl text-[#c59b5c] py-5'>Career Goals</h1>
                         <p className='text-justify md:text-lg text-[#d5d3d3]'>My ultimate goal is to become a <span className='text-[#ffef88] font-semibold'> Full Stack Developer</span>, and I have already delved into advanced tech stacks like MERN, along with mastering tools such as Tailwind CSS, Java, npm, Linux, and Redux.</p>
                     </div>
-                    <div>
+                    <div className='mb-4'>
                         <h1 className='text-3xl text-[#c59b5c] py-5'>Conclusion</h1>
                         <p className='text-justify md:text-lg text-[#d5d3d3]'>
                             I have enough to build mediate Front-End applications, now I'm exploring the power of Back-end development and trying to learn something new.</p>
+                    </div>
+                    <div className=''>
+                        <a href="https://github.com/Charan-bavaji/portfolio-/raw/main/public/CR-Resume.pdf" download="CharanRES.pdf">
+                            <button className="w-auto h-10 px-2 py-3 sm:px-4 sm:py-5 flex justify-center items-center tracking-widest text-md border-2 border-[#6a522f] rounded-xl  cursor-pointer font-bold transition duration-150 ease-in-out hover:-translate-y-2
+                              text-black hover:text-white bg-[#c59b5c] hover:bg-transparent shadow-lg text-sm">
+                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className=" mr-2 text-[16px] " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M19.903 8.586a.997.997 0 0 0-.196-.293l-6-6a.997.997 0 0 0-.293-.196c-.03-.014-.062-.022-.094-.033a.991.991 0 0 0-.259-.051C13.04 2.011 13.021 2 13 2H6c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V9c0-.021-.011-.04-.013-.062a.952.952 0 0 0-.051-.259c-.01-.032-.019-.063-.033-.093zM16.586 8H14V5.414L16.586 8zM6 20V4h6v5a1 1 0 0 0 1 1h5l.002 10H6z"></path><path d="M8 12h8v2H8zm0 4h8v2H8zm0-8h2v2H8z"></path></svg>
+                                Get Resume
+                            </button>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import SocialMedia from './SocialMedia';
 const Footer = () => {
   return (
-    <div className='w-full h-24 flex flex-col justify-center items-center gap-5 mb-4 mt-16'>
-      {/* <div className='w-[75%] h-[1px] bg-slate-400'></div> */}
+    <div className='w-full h-full py-3 flex flex-col justify-center items-center  mt-16 bg-[#c8964b]  clip '>
+      {/* <div className='w-[75%] h-[1px] bg-slate-400'></div>
       <hr className='w-[75%] h-[1px] bg-slate-900' />
       <div className='w-full h-auto flex justify-center text-center gap-5 tracking- text-white text-[18px] font-extralight'>
-        {/* <span id='font' className="font-sans font-bold text-2xl lg:text-5xl">Charan</span> */}
-        <Link to={"/about"}>
-          <h1 className='p-1  text-[#ffd28f]'>About</h1>
-        </Link>
+        <span id='font' className="font-sans font-bold text-2xl lg:text-5xl">Charan</span>
         <Link to={"/contact"}>
           <h1 className='p-1  text-[#ffd28f]'>Contact</h1>
         </Link>
+        <Link to={"/about"}>
+          <h1 className='p-1  text-[#ffd28f]'>About</h1>
+        </Link>
       </div>
-      <h2 className='tracking-wider text-white text-[18px] font-extralight'>Designed and Coded by <a href='https://github.com/Charan-bavaji' className='underline underline-offset-4 tracking-wider text-[#ffd28f]'>Charan R</a></h2>
+      <h2 className='tracking-wider text-white text-[18px] font-extralight'>Designed and Coded by <a href='https://github.com/Charan-bavaji' className='underline underline-offset-4 tracking-wider text-[#ffd28f]'>Charan R</a></h2> */}
+      <div className='flex w-full flex-col sm:flex-row justify-center items-center md:justify-evenly'>
+        <div className='p-4 tracking-wider w-auto text-start'>
+          <h1 id='font' className='text-4xl lg:text-6xl text-gray-800  '>Charan R</h1>
+          <h3 id='font' className='text-xl text-gray-800 lg:text-md font-thin'>Frontend Developer</h3>
+        </div>
+        <div className='w-auto'>
+          <span className='tracking-wider text-slate-900 font-sans'>
+            Please don't hesitate to reach out!
+          </span>
+          {/* <div className=' md:hidden p-2 w-auto'>
+            <SocialMedia flex={''} />
+          </div> */}
+          <div className='w-auto h-auto p-4 flex justify-center text-center gap-5 tracking-wider text-white text-[18px] font-extralight'>
+            <Link to='/contact'>
+              <button className="w-auto h-10 px-2 py-3 sm:px-4 sm:py-5 flex justify-center items-center text-md border-2 border-[#6a522f] rounded-xl  cursor-pointer transition duration-150 ease-in-out hover:-translate-y-2
+                              text-white backdrop-blur-sm bg-black/10 shadow-lg tracking-widest hover:text-black hover:bg-[#c59b5c]">
+                contact</button>
+            </Link>
+            <Link to='/about'>
+              <button className="w-auto h-10 px-2 py-3 sm:px-4 sm:py-5 flex justify-center items-center text-md border-2 border-[#6a522f] rounded-xl  cursor-pointer transition duration-150 ease-in-out hover:-translate-y-2
+                              text-white backdrop-blur-sm bg-black/10 shadow-lg tracking-widest hover:text-black hover:bg-[#c59b5c]">
+                about</button>
+            </Link>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <h2 className='tracking-wider text-white text-[18px] font-extralight'>Designed and Coded by <a href='https://github.com/Charan-bavaji' className='underline underline-offset-4 tracking-wider text-slate-900'>Charan R</a></h2>
+      </div>
     </div>
   )
 }
