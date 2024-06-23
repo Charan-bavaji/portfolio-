8637d38117c19883f9929108fd7f42d85b92d423
Charan-bavaji <charan.r7760@gmail.com>
2024-06-23 07:37:48 +0530

    edited links

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -101,12 +101,12 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className='w-full flex justify-center items-center'>
-                    <div className=' group w-[70%] flex justify-center items-center gap-3 bg-[#c59b5c] hover:bg-transparent border-2 border-[#6a522f] rounded-full font-bold tracking-wider transition duration-150 ease-in-out hover:translate-y-2 cursor-pointer'>
+                    <a className=' group w-[70%] flex justify-center items-center gap-3 bg-[#c59b5c] hover:bg-transparent border-2 border-[#6a522f] rounded-full font-bold tracking-wider transition duration-150 ease-in-out hover:translate-y-2 cursor-pointer' href="https://github.com/Charan-bavaji?tab=repositories">
                         <h1 className='text-lg font-bold text-black py-3 rounded-full w-auto  group-hover:bg-transparent group-hover:text-white'>
                             VIEW MORE
                         </h1>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="mr-1 text-sm md:text-[16px] lg:text-lg group-hover:text-white" height="1.3em" width="1.3em" xmlns="http://www.w3.org/2000/svg"><path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path><path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path></svg>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
